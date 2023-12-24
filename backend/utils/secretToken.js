61814018e90adc5adf2093f488dd58680d2905e2
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-12-24 10:11:22 +0800

    fix: JWT secrect key undefined

--- backend/utils/secretToken.js
+++ backend/utils/secretToken.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const createSecretToken = (id) => {
-    return jwt.sign({ id }, process.env.TOKEN_KEY, {
-        expiresIn: 3 * 24 * 60 * 60,
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN  
     });
 };
 
