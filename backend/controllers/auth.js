d7ffdc46f504b043b5b8c382fbf6f92576662d4b
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-12-24 11:44:36 +0800

    feat: added login controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,8 +1,9 @@
 const UserModel = require('../models/users');
 const createSecretToken = require('../utils/secretToken');
+const bcrypt = require('bcryptjs');
 
-//signUp controller
-exports.signup = async (req, res ) => {
+//signup controller
+exports.signup = async (req, res) => {
     try {
         const { email, username, password, createdAt } = req.body;//get user details from the req body
 
@@ -30,3 +31,30 @@ exports.signup = async (req, res ) => {
     }
 }
 
+//login controller 
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) return res.json({ message: "All fields are required" });
+
+        //check if email is correct
+        const user = await UserModel.findOne({ email });
+        if (!user) return res.json({ message: "Incorrect email or password" });
+
+        //check if password is correct
+        const auth = await bcrypt.compare(password, user.password);
+        if (!auth) return res.json({ message: "Incorrect email or password" });
+
+        //create a cookie for the json web token
+        const token = createSecretToken(user._id);
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: false
+        })
+
+        res.status(201).json({ message: "User logged in successfully", success: true, user })
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
