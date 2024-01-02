31f6dbee06750dccd0d6efe6d2da8fba4af19258
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2024-01-02 19:55:22 +0800

    feat: added routing of pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Home, Login, Signup } from "./pages";
+
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </>
   );
 }
