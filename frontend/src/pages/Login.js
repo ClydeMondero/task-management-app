48aa8d687dba0ba0c141039acb8024ea47bba14c
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2024-01-03 22:00:31 +0800

    feat: login page template

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,25 @@
 function Login() {
     return (
-        <>
-            This is the login page
-        </>
+        <div className="flex items-center justify-center h-lvh">
+            <div className="flex flex-col gap-8 p-8 bg-secondary-bg h-3/4 w-1/3 rounded-lg">
+                <p className="self-center text-3xl font-bold text-primary">Welcome to TASQ</p>
+                <form className="flex flex-col gap-4">
+                    <input className="input" type="text" placeholder="Email or Username" />
+                    <input className="input" type="password" placeholder="Password" />
+
+                    <div className="flex flex-col gap-4">
+                        <p className="text-secondary">Forgot your password?</p>
+                        <input className="bg-primary text-white text-xl font-bold w-full py-2 px-4 rounded-lg" type="submit" value="LOGIN" />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <p>By continuing you agree to TASQ’s <span className="text-secondary">Terms of service and privacy policy.</span></p>
+                        <hr className="bg-primary h-[0.20rem]" />
+                        <p className="self-center">Not on TASQ yet? <span className="text-secondary">Signup now</span></p>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "secondary-bg": "rgba(255, 209, 227, 0.75)",
+        "primary": "rgba(57, 36, 103, 1)",
+        "secondary": "rgba(163, 103, 177, 1)"
+      },
+    },
   },
   plugins: [],
 }
